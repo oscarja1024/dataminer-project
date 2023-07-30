83dfb27c014ad6a7536ca1aaf82dfccc9f5746cd
osky <oscarja1024@gmail.com>
2023-07-30 11:05:19 +0200

    fix

--- src/main/java/com/oscarjimenez/dataminerproject/api/controller/MinerController.java
+++ src/main/java/com/oscarjimenez/dataminerproject/api/controller/MinerController.java
@@ -30,14 +30,14 @@ public class MinerController {
     @Autowired
     GetMetadataService getMetadataService;
 
-    @GetMapping(path="/metadata", produces = "application/json")
+    @PostMapping(path="/metadata", produces = "application/json")
     public MetadataResponseDTO getMetadata(){
         return getMetadataService
                 .getAllMetadata(MetadataRequest.builder().locale(Constants.LOCALE)
                         .build());
     }
 
-    @GetMapping(path="/cardBacksById",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardBacksById",consumes = "application/json" , produces = "application/json")
     public CardBacksResponseDTO getCardsBackById(MinerDTO request){
         return getCardsBacksService
                 .getCardBacksById(CardBackRequest.builder()
@@ -45,27 +45,27 @@ public class MinerController {
                         .build());
     }
 
-    @GetMapping(path="/cardBacksByCategory",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardBacksByCategory",consumes = "application/json" , produces = "application/json")
     public CardBacksResponseDTO getCardBacksByCategory(MinerDTO request){
         return getCardsBacksService.getCardBacksByCardBackCategory(CardBackRequest.builder()
                 .category(request.getParams().get(Constants.CATEGORY))
                 .page(request.getPage()).build());
     }
 
-    @GetMapping(path="/cardBacks",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardBacks",consumes = "application/json" , produces = "application/json")
     public CardBacksResponseDTO getCardBacks(MinerDTO request){
         return getCardsBacksService.getCardBacks(CardBackRequest.builder()
                 .page(request.getPage()).build());
     }
 
-    @GetMapping(path="/cardBacksSort",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardBacksSort",consumes = "application/json" , produces = "application/json")
     public CardBacksResponseDTO getCardBacksSort(MinerDTO request){
         return getCardsBacksService.getCardBacksSort(CardBackRequest.builder()
                 .sort(request.getParams().get(Constants.SORT))
                 .page(request.getPage()).build());
     }
 
-    @GetMapping(path="/cardBacksByCategorySort",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardBacksByCategorySort",consumes = "application/json" , produces = "application/json")
     public CardBacksResponseDTO getCardBacksCategorySort(MinerDTO request){
         return getCardsBacksService.getCardBacksByCardBackCategorySort(CardBackRequest.builder()
                 .sort(request.getParams().get(Constants.SORT))
@@ -73,7 +73,7 @@ public class MinerController {
                 .page(request.getPage()).build());
     }
 
-    @GetMapping(path="/decksByCardsAndHero",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/decksByCardsAndHero",consumes = "application/json" , produces = "application/json")
     public DeckDTO getDeckByCardListAndHero(MinerDTO request){
         return getDecksService.getDeckByCardListAndHero(DeckRequestDTO.builder()
                 .ids(request.getCardIds())
@@ -81,7 +81,7 @@ public class MinerController {
                 .build());
     }
 
-    @GetMapping(path="/decksByCards",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/decksByCards",consumes = "application/json" , produces = "application/json")
     public DeckDTO getDeckByCardListAutoHero(MinerDTO request){
         return getDecksService.getDeckByCardListAutoHero(DeckRequestDTO.builder()
                 .ids(request.getCardIds())
@@ -108,21 +108,21 @@ public class MinerController {
                 .page(request.getPage()).build());
     }
 
-    @GetMapping(path="/cardsSort",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsSort",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsSort(MinerDTO request){
         return getCardsService.getAllCardsSort(CardRequestDTO.builder()
                 .page(request.getPage())
                 .sort(request.getParams().get(Constants.SORT)).build());
     }
 
-    @GetMapping(path="/cardsPageSize",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsPageSize",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsSetPageSize(MinerDTO request){
         return getCardsService.getAllCardsSetPageSize(CardRequestDTO.builder()
                 .page(request.getPage())
                 .pageSize(request.getParams().get(Constants.PAGE_SIZE)).build());
     }
 
-    @GetMapping(path="/cardsPageSizeSort",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsPageSizeSort",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByPageSetPageSizeSort(MinerDTO request){
         return getCardsService.getAllCardsByPageSetPageSizeSort(CardRequestDTO.builder()
                 .page(request.getPage())
@@ -130,14 +130,14 @@ public class MinerController {
                 .sort(request.getParams().get(Constants.SORT)).build());
     }
 
-    @GetMapping(path="/cardsByManaCost",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByManaCost",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByManaCost(MinerDTO request){
         return getCardsService.getAllCardsByManaCost(CardRequestDTO.builder()
                 .page(request.getPage())
                 .mana(request.getParams().get(Constants.MANA)).build());
     }
 
-    @GetMapping(path="/cardsByManaCostAndAttack",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByManaCostAndAttack",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByManaCostAndAttack(MinerDTO request){
         return getCardsService.getAllCardsByManaCostAndAttack(CardRequestDTO.builder()
                 .page(request.getPage())
@@ -145,21 +145,21 @@ public class MinerController {
                 .attack(request.getParams().get(Constants.ATTACK)).build());
     }
 
-    @GetMapping(path="/cardsByAttack",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByAttack",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByAttack(MinerDTO request){
         return getCardsService.getAllCardsByAttack(CardRequestDTO.builder()
                 .page(request.getPage())
                 .attack(request.getParams().get(Constants.ATTACK)).build());
     }
 
-    @GetMapping(path="/cardsByType",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByType",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByType(MinerDTO request){
         return getCardsService.getAllCardsByType(CardRequestDTO.builder()
                 .page(request.getPage())
                 .type(request.getParams().get(Constants.CARD_TYPE)).build());
     }
 
-    @GetMapping(path="/cardsByAttackAndType",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByAttackAndType",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByTypeAndAttack(MinerDTO request){
         return getCardsService.getAllCardsByTypeAndAttack(CardRequestDTO.builder()
                 .page(request.getPage())
@@ -167,7 +167,7 @@ public class MinerController {
                 .attack(request.getParams().get(Constants.ATTACK)).build());
     }
 
-    @GetMapping(path="/cardsByAttackAndManaCost",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByAttackAndManaCost",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByTypeAndManaCost(MinerDTO request){
         return getCardsService.getAllCardsByTypeAndManaCost(CardRequestDTO.builder()
                 .page(request.getPage())
@@ -175,7 +175,7 @@ public class MinerController {
                 .mana(request.getParams().get(Constants.MANA)).build());
     }
 
-    @GetMapping(path="/cardsByAttackAndManaCostAndType",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByAttackAndManaCostAndType",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByTypeAndAttackAndManaCost(MinerDTO request){
         return getCardsService.getAllCardsByTypeAndAttackAndManaCost(CardRequestDTO.builder()
                 .page(request.getPage())
@@ -184,28 +184,28 @@ public class MinerController {
                 .attack(request.getParams().get(Constants.ATTACK)).build());
     }
 
-    @GetMapping(path="/cardsByHealth",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByHealth",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByHealth(MinerDTO request){
         return getCardsService.getAllCardsByHealth(CardRequestDTO.builder()
                 .page(request.getPage())
                 .health(request.getParams().get(Constants.HEALTH)).build());
     }
 
-    @GetMapping(path="/cardsByGameMode",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsByGameMode",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsByGameMode(MinerDTO request){
         return getCardsService.getAllCardsByGameMode(CardRequestDTO.builder()
                 .page(request.getPage())
                 .gameMode(request.getParams().get(Constants.GAME_MODE)).build());
     }
 
-    @GetMapping(path="/cardsBySpellSchool",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsBySpellSchool",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsBySpellSchool(MinerDTO request){
         return getCardsService.getAllCardsBySpellSchool(CardRequestDTO.builder()
                 .page(request.getPage())
                 .spellSchool(request.getParams().get(Constants.SPELL_SCHOOL)).build());
     }
 
-    @GetMapping(path="/cardsBySet",consumes = "application/json" , produces = "application/json")
+    @PostMapping(path="/cardsBySet",consumes = "application/json" , produces = "application/json")
     public GetCardsResponseDTO getAllCardsBySet(MinerDTO request){
         return getCardsService.getAllCardsBySet(CardRequestDTO.builder()
                 .page(request.getPage())
